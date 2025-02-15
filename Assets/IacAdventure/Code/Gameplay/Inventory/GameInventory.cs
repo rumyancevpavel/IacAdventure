a9c812c74ffbd9cdb2cd08e57a2e7e0493559452
Pavel Rumyancev <paulrumyancev@gmail.com>
2025-02-15 12:43:03 +0200

    Treasure Cube

---
+++ Assets/IacAdventure/Code/Gameplay/Inventory/GameInventory.cs
@@ -0,0 +1,24 @@
+namespace IacAdventure.Gameplay.Inventory
+{
+	public class GameInventory
+	{
+		public static int GoldAmount { get; private set; }
+		public static int GemsAmount { get; private set; }
+		public static int ExperienceAmount { get; private set; }
+
+		public static void AddGold(int amount)
+		{
+			GoldAmount += amount;
+		}
+
+		public static void AddExperience(int amount)
+		{
+			ExperienceAmount += amount;
+		}
+
+		public static void AddGems(int amount)
+		{
+			GemsAmount += amount;
+		}
+	}
+}
---
+++ Assets/IacAdventure/Code/Gameplay/Items/TreasureCube.cs
@@ -0,0 +1,33 @@
+using IacAdventure.Gameplay.Inventory;
+using UnityEngine;
+
+namespace IacAdventure.Gameplay.Items
+{
+	public class TreasureCube : MonoBehaviour
+	{
+		#region Editor Exposed
+
+		[SerializeField]
+		private int _experiencePoints;
+
+		[SerializeField]
+		private int _goldAmount;
+
+		[SerializeField]
+		private int _gemsAmount;
+
+		#endregion
+
+		#region Methods
+
+		public void PutIntoInventory()
+		{
+			Debug.Log("Put into inventory");
+			GameInventory.AddGold(_goldAmount);
+			GameInventory.AddExperience(_experiencePoints);
+			GameInventory.AddGems(_gemsAmount);
+		}
+
+		#endregion
+	}
+}
